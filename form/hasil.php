23e2bc07ca0081165735830d988a1cd7cdff890a
RakaYuda <30462016+RakaYuda@users.noreply.github.com>
2017-10-24 22:20:16 +0700

    Add files via upload

--- form/hasil.php
+++ form/hasil.php
@@ -1,25 +1,34 @@
 <html>
 <head>
-<title>Untitled Document</title>
+	<title>Untitled Document</title>
 </head>
 
 <body>
-<center>Akun Anda Berhasil Dibuat<br>
-<table>
-	<?php
-		$nama=$_POST["nama"];
-		$user=$_POST["user"];
-		$email=$_POST["email"];
-		$notelepon=$_POST["notelepon"];
-		$password=$_POST["password"];
-		$password=str_repeat("*",strlen("password"));
+	<center>Akun Anda Berhasil Dibuat<br>
+		<table>
+			<?php
+			$nama=$_POST["nama"];
+			$user=$_POST["user"];
+			$email=$_POST["email"];
+			$notelepon=$_POST["notelepon"];
+			$password=$_POST["password"];
+			$password=str_repeat("*",strlen("password"));
+			$DBServer = "localhost";
+			$DBUser = "root";
+			$DBPass = "";
+			$DBName ="instagram";
+			$conn = new mysqli($DBServer, $DBUser, $DBPass, $DBName);
 
-		echo"<tr><td>Nama</td><td>:</td><td>$nama</td></tr>";
-		echo"<tr><td>Nama Pengguna</td><td>:</td><td>$user</td></tr>";
-		echo"<tr><td>Email</td><td>:</td><td>$email</td></tr>";
-		echo"<tr><td>No Telepon</td><td>:</td><td>$notelepon</td></tr>";
-		echo"<tr><td>Password</td><td>:</td><td>$password</td></tr>";
-	?>
-</table>
-</body>
-</html>
+			if ($conn) {
+				trigger_error('Database connection failed: '  . $conn->connect_error, E_USER_ERROR);
+			}
+			echo"<tr><td>Nama</td><td>:</td><td>$nama</td></tr>";
+			echo"<tr><td>Nama Pengguna</td><td>:</td><td>$user</td></tr>";
+			echo"<tr><td>Email</td><td>:</td><td>$email</td></tr>";
+			echo"<tr><td>No Telepon</td><td>:</td><td>$notelepon</td></tr>";
+			echo"<tr><td>Password</td><td>:</td><td>$password</td></tr>";
+
+			?>
+		</table>
+	</body>
+	</html>
